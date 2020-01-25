a31b4f6167c1cd190d53a18e822ea73d74deaea0
sue445 <sue445@sue445.net>
2020-01-26 00:17:22 +0900

    Add hub params

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
-#!/bin/sh -l
+#!/bin/sh -le
 
-echo "hello $1"
+export GITHUB_TOKEN=$1
+readonly REVIEWER=$2
+readonly ASSIGN=$3
+readonly MILESTONE=$4
+readonly DRAFT=$5
+
+echo "REVIEWER=$REVIEWER"
+echo "ASSIGN=$ASSIGN"
+echo "MILESTONE=$MILESTONE"
+echo "DRAFT=$DRAFT"
