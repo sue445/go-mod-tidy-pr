bd35a16c7f251c89c83455cb2840ea2a98cf4e23
sue445 <sue445@sue445.net>
2020-01-26 00:47:57 +0900

    Add go_mod_directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,13 @@ readonly REVIEWER=$2
 readonly ASSIGN=$3
 readonly MILESTONE=$4
 readonly DRAFT=$5
+readonly GO_MOD_DIRCTORY=$6
 
 echo "REVIEWER=$REVIEWER"
 echo "ASSIGN=$ASSIGN"
 echo "MILESTONE=$MILESTONE"
 echo "DRAFT=$DRAFT"
+echo "GO_MOD_DIRCTORY=$GO_MOD_DIRCTORY"
+
+cd $GO_MOD_DIRCTORY
+pwd
